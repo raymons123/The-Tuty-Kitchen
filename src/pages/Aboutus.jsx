2b8b8cf0744b88489e4116838340472ea823a35a
Raymons sudev S <ed25b047@smail.iitm.ac.in>
2026-09-08 03:47:55 +0530

    completed the About page structure

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -1,7 +1,23 @@
 function Aboutus() {
   return (
-    <div>
-      <h1>About Us</h1>
+    <div className="min-h-screen">
+      <div className="flex justofy-center gap-15">
+        <div className="w-3/5 max-w-2xl mx-auto">
+          <h1 className="text-5xl text-center p-15">The Tuty Kitchen</h1>
+          <p className="text-center p-2">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit culpa quasi delectus. Modi temporibus assumenda accusamus voluptas deserunt dolorum, inventore reiciendis, cupiditate enim ut illum optio iste! Provident, sint consequuntur!
+          </p>
+          <p className="text-center p-10">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit culpa quasi delectus. Modi temporibus assumenda accusamus voluptas deserunt dolorum, inventore reiciendis, cupiditate enim ut illum optio iste! Provident, sint consequuntur!
+          </p>
+          <p className="text-center p-10">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit culpa quasi delectus. Modi temporibus assumenda accusamus voluptas deserunt dolorum, inventore reiciendis, cupiditate enim ut illum optio iste! Provident, sint consequuntur!
+          </p>
+        </div>
+        <div className="w-2/5">
+          <p>Owners Photo</p>
+        </div>
+      </div>
     </div>
   )
 }
